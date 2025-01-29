34f2ef74392172185aebd597d635427678b7b241
Anurag Uranw <skanuraguranw12@gmail.com>
2025-01-29 21:20:08 +0530

    add code

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -5,10 +5,10 @@ import delete_icon from "../assets/delete.png";
 
 const TodoItems = ({ text, id, isComplete, deleteTodo, toggle }) => {
   return (
-    <div onClick={() => { toggle(id) }} className="w-full flex items-center justify-between gap-1 my-3 bg-red-500">
+    <div onClick={() => { toggle(id) }} className="w-full flex items-center justify-between gap-2 my-3">
       <div className="flex items-center gap-1">
         <img src={isComplete ? tick : not_tick} alt="icon" className="w-6" />
-        <p className={`text-white/80 ml-3 text-[1.1rem] break-all ${isComplete ? "line-through" : ""}`}>{text}</p>
+        <p className={`text-white/80 ml-3 text-[1.1rem] text-wrap break-all ${isComplete ? "line-through" : ""}`} style={{ wordBreak: 'break-word', whiteSpace: 'normal' }}>{text}</p>
       </div>
       <img onClick={() => { deleteTodo(id) }} src={delete_icon} alt="icon" className="w-5 cursor-pointer" />
     </div>
